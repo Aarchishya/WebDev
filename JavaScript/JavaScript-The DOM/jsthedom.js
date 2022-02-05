885afc98767b2ac94074e327e27c81a55a800c5b
Aarchishya <59142633+Aarchishya@users.noreply.github.com>
2022-02-05 16:16:44 +0530

    Update jsthedom.js

--- JavaScript/JavaScript-The DOM/jsthedom.js
+++ JavaScript/JavaScript-The DOM/jsthedom.js
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div id="main" class="container">
-        <ul>
+        <ul id="nav">
             <li>Home</li>
             <li>About Us</li>
             <li>Contact Us</li>
@@ -16,6 +16,23 @@
             <li>More Help</li>
         </ul>
     </div>
-    
+    <div class="container">
+        Another Container
+    </div>
+    <script>
+        let main = document.getElementById('main');
+        console.log(main);
+        let nav = document.getElementById('nav');
+        console.log(nav);
+        let container=document.getElementsByClassName('container');
+        console.log(container);
+        // let sel = document.querySelector('.container');
+        // console.log("Selector Return",sel);
+        // let sel = document.querySelector('#nav>li');
+        // console.log("Selector Return",sel);
+        let sels = document.querySelectorAll('#nav>li');
+        console.log("Selector Return",sels);
+
+    </script>
 </body>
 </html>
